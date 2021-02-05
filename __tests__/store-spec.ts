0088ee93c2a2866988ef7ed2451f7518b97e2408
calidion <calidion@gmail.com>
2021-02-05 11:36:24 +0800

    enabled store

--- __tests__/store-spec.ts
+++ __tests__/store-spec.ts
@@ -1,12 +1,16 @@
 import { MemoryStore } from "../src/index";
 
-test("Should create object", async() => {
+test("Should create object", async () => {
   expect(MemoryStore).toBeTruthy();
 
   const store = new MemoryStore();
   const id = "aaa";
   let data = await store.get(id);
   expect(data).toBeFalsy();
-  await store.set(id, {sss:11});
-  data = store.get(id);
+  await store.set(id, { sss: 11 });
+  data = await store.get(id);
+  expect(data.sss === 11).toBeTruthy();
+  await store.destroy(id);
+  data = await store.get(id);
+  expect(data === undefined).toBeTruthy();
 });
